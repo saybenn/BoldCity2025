4551c4a8f3fc0386f61ff74f2d3153aa8cd6572c
saybencodes <saybencodes@gmail.com>
2026-05-10 16:39:38 -0400

    done

--- components/ValueProps.jsx
+++ components/ValueProps.jsx
@@ -27,7 +27,7 @@ export default function ValueProps() {
   return (
     <section className="relative z-20 hidden h-0 sm:block">
       <div className="mx-auto max-w-6xl px-4 -translate-y-1/2 lg:-translate-y-[58%]">
-        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-5 sm:hidden lg:grid-cols-3 lg:grid">
           {valueProps.map((item) => (
             <div
               key={item.title}
